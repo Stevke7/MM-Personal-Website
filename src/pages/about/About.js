1b5d24e6290281600080989385f179fd180d2913
Ljubisa <stevanovicljubisa07@gmail.com>
2022-08-22 20:48:07 +0200

    for initial

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,17 +1,16 @@
-import {Col, Container, Row} from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 
 function About() {
-
-    return (
-        <>
-            <Container>
-                <Container>
-                    <h1>About is under construction</h1>
-                </Container>
-            </Container>
-        </>
-    )
-
+	return (
+		<>
+			<Container>
+				<Container>
+					<h1>About is under construction</h1>
+					<h2>samo za init</h2>
+				</Container>
+			</Container>
+		</>
+	);
 }
 
-export default About
+export default About;
