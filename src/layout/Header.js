1be1557c8eb39a3a114c1bc5ad433d87c46bbee4
Stevanovic <stevanovicljubisa07@gmail.com>
2022-09-09 00:07:23 +0200

    beautifying contact page

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,182 +1,219 @@
-import {Col, Container, Form, Image, Nav, Offcanvas, Row} from "react-bootstrap";
-import {NavLink, useLocation} from "react-router-dom";
+import {
+	Col,
+	Container,
+	Form,
+	Image,
+	Nav,
+	Offcanvas,
+	Row,
+} from "react-bootstrap";
+import { NavLink, useLocation } from "react-router-dom";
 import ReactSwitch from "react-switch";
-import {useEffect, useState} from "react";
-import {FormControlLabel, FormGroup, styled, Switch} from "@mui/material";
-import {useMediaQuery} from "react-responsive";
+import { useEffect, useState } from "react";
+import { FormControlLabel, FormGroup, styled, Switch } from "@mui/material";
+import { useMediaQuery } from "react-responsive";
 
-
-const MaterialUISwitch = styled(Switch)(({theme}) => ({
-    width: 62,
-    height: 34,
-    padding: 7,
-    '& .MuiSwitch-switchBase': {
-        margin: 1,
-        padding: 0,
-        transform: 'translateX(6px)',
-        '&.Mui-checked': {
-            color: '#fff',
-            transform: 'translateX(22px)',
-            '& .MuiSwitch-thumb:before': {
-                backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-                    '#fff',
-                )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
-            },
-            '& + .MuiSwitch-track': {
-                opacity: 1,
-                backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
-            },
-        },
-    },
-    '& .MuiSwitch-thumb': {
-        backgroundColor: theme.palette.mode === 'dark' ? '#003892' : '#001e3c',
-        width: 32,
-        height: 32,
-        '&:before': {
-            content: "''",
-            position: 'absolute',
-            width: '100%',
-            height: '100%',
-            left: 0,
-            top: 0,
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'center',
-            backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-                '#fff',
-            )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
-        },
-    },
-    '& .MuiSwitch-track': {
-        opacity: 1,
-        backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
-        borderRadius: 20 / 2,
-    },
+const MaterialUISwitch = styled(Switch)(({ theme }) => ({
+	width: 62,
+	height: 34,
+	padding: 7,
+	"& .MuiSwitch-switchBase": {
+		margin: 1,
+		padding: 0,
+		transform: "translateX(6px)",
+		"&.Mui-checked": {
+			color: "#fff",
+			transform: "translateX(22px)",
+			"& .MuiSwitch-thumb:before": {
+				backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+					"#fff"
+				)}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
+			},
+			"& + .MuiSwitch-track": {
+				opacity: 1,
+				backgroundColor: theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
+			},
+		},
+	},
+	"& .MuiSwitch-thumb": {
+		backgroundColor: theme.palette.mode === "dark" ? "#003892" : "#001e3c",
+		width: 32,
+		height: 32,
+		"&:before": {
+			content: "''",
+			position: "absolute",
+			width: "100%",
+			height: "100%",
+			left: 0,
+			top: 0,
+			backgroundRepeat: "no-repeat",
+			backgroundPosition: "center",
+			backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+				"#fff"
+			)}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
+		},
+	},
+	"& .MuiSwitch-track": {
+		opacity: 1,
+		backgroundColor: theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
+		borderRadius: 20 / 2,
+	},
 }));
 
 function Header(props) {
-    let {setTheme, id} = props
-    const isMobile = useMediaQuery({query: `(max-width: 1024px)`});
-    let tabs = {
-        home: "",
-        work: "work",
-        contact: "contact"
-    }
+	let { setTheme, id } = props;
+	const isMobile = useMediaQuery({ query: `(max-width: 1024px)` });
+	let tabs = {
+		home: "",
+		work: "work",
+		contact: "contact",
+	};
 
+	const [show, setShow] = useState(false);
 
-    const [show, setShow] = useState(false);
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+	const toggleTheme = () => {
+		setTheme((curr) => (curr === "light" ? "dark" : "light"));
+	};
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    const toggleTheme = () => {
-        setTheme((curr) => (curr === "light" ? "dark" : "light"));
-    };
+	return (
+		<div>
+			{!isMobile && (
+				<Container className="px-5">
+					<Row className="py-3 justify-content-between align-items-center">
+						<Col className="d-flex align-items-center " md={4}>
+							<img
+								src={`${
+									id === "light"
+										? "/images/Logo/Logo.svg"
+										: "images/Logo/logo-whitesvg.svg"
+								}`}
+								alt="logo"
+							/>
+						</Col>
+						<Col md={4}>
+							<nav className="d-flex justify-content-center m-0 ">
+								<div className="nav-link">
+									<NavLink
+										// className="text-black"
+										to={tabs.home}
+									>
+										Home
+										<div className="line"></div>
+									</NavLink>
+								</div>
+								<div className="nav-link mx-5  ">
+									<NavLink
+										// className="text-black"
+										to={tabs.work}
+									>
+										Work
+										<div className="line"></div>
+									</NavLink>
+								</div>
+								<div className="nav-link">
+									<NavLink
+										// className="text-black"
+										to={tabs.contact}
+									>
+										Contact
+										<div className="line"></div>
+									</NavLink>
+								</div>
+							</nav>
+						</Col>
+						<Col md={4} className="d-flex justify-content-end w-25">
+							<FormGroup>
+								<FormControlLabel
+									onClick={toggleTheme}
+									control={<MaterialUISwitch />}
+								/>
+							</FormGroup>
+						</Col>
+					</Row>
+				</Container>
+			)}
+			{/*Different layout for mobile screen*/}
 
-    return (
-        <div>
-            {!isMobile &&
-                <Container className='px-5'>
-                    <Row className="py-3 justify-content-between align-items-center">
-                        <Col className="d-flex align-items-center " md={4}>
-                            <img src={`${id === 'light' ? '/images/Logo/Logo.svg' : 'images/Logo/logo-whitesvg.svg'}`}
-                                 alt="logo"/>
-                        </Col>
-                        <Col md={4}>
-                            <nav className='d-flex justify-content-center m-0 '>
-                                <div className="nav-link">
-                                    <NavLink
-                                        // className="text-black"
-                                        to={tabs.home}>Home
-                                        <div className="line"></div>
-                                    </NavLink>
-                                </div>
-                                <div className="nav-link mx-5  ">
-                                    <NavLink
-                                        // className="text-black"
-                                        to={tabs.work}>Work
-                                        <div className="line"></div>
-                                    </NavLink>
-                                </div>
-                                <div className="nav-link">
-                                    <NavLink
-                                        // className="text-black"
-                                        to={tabs.contact}>Contact
-                                        <div className="line"></div>
-                                    </NavLink>
-                                </div>
-                            </nav>
-                        </Col>
-                        <Col md={4} className="d-flex justify-content-end w-25">
-                            <FormGroup>
-                                <FormControlLabel onClick={toggleTheme}
-                                                  control={<MaterialUISwitch sx={{m: 1}}/>}
-                                />
-                            </FormGroup>
-                        </Col>
-                    </Row>
-                </Container>
-            }
-            {/*Different layout for mobile screen*/}
-
-            {isMobile &&
-                <>
-                    <Container>
-                        <div className="py-3 d-flex justify-content-between align-items-center">
-                            <Image src='/images/Logo/Logo.svg' alt="logo"/>
-                            <Image className='hamburger' onClick={handleShow}
-                                   src='/icons/hamburger.svg'/>
-                        </div>
-                    </Container>
-                    {show && <div className={`mobileMenu position-relative `}>
-                        <Offcanvas className='toggle' placement={"end"} show={show}
-                                   onHide={handleClose}
-                                   backdrop="static">
-                            <Offcanvas.Header closeButton></Offcanvas.Header>
-                            <Offcanvas.Body>
-                                <div className="d-flex flex-column justify-content-between my-5  h-85">
-                                    <p className='border-bottom pb-3 text-white'>NAVIGATION</p>
-                                    <nav id='mobile' className='d-flex flex-column align-items-start m-0 '>
-                                        <div className="nav-link">
-                                            <NavLink
-                                                onClick={handleClose}
-                                                className="text-white"
-                                                to={tabs.home}>Home
-                                            </NavLink>
-                                        </div>
-                                        <div className="nav-link   ">
-                                            <NavLink
-                                                onClick={handleClose}
-                                                className="text-white"
-                                                to={tabs.work}>Work
-                                            </NavLink>
-                                        </div>
-                                        <div className="nav-link">
-                                            <NavLink
-                                                onClick={handleClose}
-                                                className="text-white"
-                                                to={tabs.contact}>Contact
-                                            </NavLink>
-                                        </div>
-                                    </nav>
-                                    <div className="d-flex flex-column text-white align-items-start ">
-                                        <p className='border-top text-white pt-3 w-100'> SOCIAL</p>
-                                        <div className="d-flex align-items-center justify-content-between w-100">
-                                            <p>Instagram</p>
-                                            <p>Behance</p>
-                                            <p>Dribble</p>
-                                            <p>LinkedIn</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </Offcanvas.Body>
-                        </Offcanvas>
-                    </div>}
-
-                </>
-            }
-
-        </div>
-    )
+			{isMobile && (
+				<>
+					<Container>
+						<div className="py-3 d-flex justify-content-between align-items-center">
+							<Image src="/images/Logo/Logo.svg" alt="logo" />
+							<Image
+								className="hamburger"
+								onClick={handleShow}
+								src="/icons/hamburger.svg"
+							/>
+						</div>
+					</Container>
+					{show && (
+						<div className={`mobileMenu position-relative `}>
+							<Offcanvas
+								className="toggle"
+								placement={"end"}
+								show={show}
+								onHide={handleClose}
+								backdrop="static"
+							>
+								<Offcanvas.Header closeButton></Offcanvas.Header>
+								<Offcanvas.Body>
+									<div className="d-flex flex-column justify-content-between my-5  h-85">
+										<p className="border-bottom pb-3 text-white">NAVIGATION</p>
+										<nav
+											id="mobile"
+											className="d-flex flex-column align-items-start m-0 "
+										>
+											<div className="nav-link">
+												<NavLink
+													onClick={handleClose}
+													className="text-white"
+													to={tabs.home}
+												>
+													Home
+												</NavLink>
+											</div>
+											<div className="nav-link   ">
+												<NavLink
+													onClick={handleClose}
+													className="text-white"
+													to={tabs.work}
+												>
+													Work
+												</NavLink>
+											</div>
+											<div className="nav-link">
+												<NavLink
+													onClick={handleClose}
+													className="text-white"
+													to={tabs.contact}
+												>
+													Contact
+												</NavLink>
+											</div>
+										</nav>
+										<div className="d-flex flex-column text-white align-items-start ">
+											<p className="border-top text-white pt-3 w-100">
+												{" "}
+												SOCIAL
+											</p>
+											<div className="d-flex align-items-center justify-content-between w-100">
+												<p>Instagram</p>
+												<p>Behance</p>
+												<p>Dribble</p>
+												<p>LinkedIn</p>
+											</div>
+										</div>
+									</div>
+								</Offcanvas.Body>
+							</Offcanvas>
+						</div>
+					)}
+				</>
+			)}
+		</div>
+	);
 }
 
-export default Header
-
+export default Header;
