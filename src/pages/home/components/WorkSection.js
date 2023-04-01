aae8173ea3d5c5b8a2cb070b3ea2e9346cb1192a
Stevanovic <stevanovicljubisa07@gmail.com>
2023-04-02 00:00:03 +0200

    Modal overlay setup

--- src/pages/home/components/WorkSection.js
+++ src/pages/home/components/WorkSection.js
@@ -1,10 +1,55 @@
 import { Container, Row, Col, Card } from "react-bootstrap";
-//import { Card } from "react-bootstrap/Card";
+import ModalOverlay from "./modal-overlay/modal-overlay";
 import { useMediaQuery } from "react-responsive";
 import { BiPlus } from "react-icons/bi";
+import { useState } from "react";
+
+const cardsData = [
+	{
+		id: 1,
+		title: "Cheetah Atletic",
+		description: "Logo design",
+		image: "/images/Desktop/cheetah-cover.png",
+		modalContent: "Content for Cheetah Atletic modal",
+	},
+	{
+		id: 2,
+		title: "Trekker",
+		description: "UI/UX Design",
+		image: "/images/Desktop/trekker-cover.png",
+		modalContent: "Content for Trekker modal",
+	},
+	{
+		id: 3,
+		title: "Ecco Energy",
+		description: "Logo Design",
+		image: "/images/Desktop/ecoenergy-cover.png",
+		modalContent: "Content for Ecco Energy modal",
+	},
+	{
+		id: 4,
+		title: "Mindful Sould",
+		description: "Website redesign",
+		image: "/images/Desktop/mindfulsouls-cover.png",
+		modalContent: "Content for Mindful Sould modal",
+	},
+];
 
 function WorkSection() {
 	const isMobile = useMediaQuery({ query: `(max-width: 1140px)` });
+
+	const [showModal, setShowModal] = useState(false);
+	const [modalContent, setModalContent] = useState("");
+
+	const handleCloseModal = () => {
+		setShowModal(false);
+		console.log("ZATVORENOOO");
+	};
+	const handleOpenModal = (content) => {
+		setShowModal(true);
+		setModalContent(content);
+		console.log("SEZAME OPEN");
+	};
 
 	return (
 		<>
@@ -14,68 +59,56 @@ function WorkSection() {
 						<h1 className="text-center pt-5 pb-5">A Selection of my work</h1>
 					</Row>
 					<Row style={{ height: "375px", marginBottom: "15px" }}>
-						<Col md={6}>
-							<Card
-								className="card"
-								style={{
-									backgroundImage: "url('/images/Desktop/cheetah-cover.png')",
-								}}
-							>
-								<Card.Body>
-									<Card.Title className="card-title">
-										Cheetah Atletic
-									</Card.Title>
-									<Card.Text className="card-text">Logo design</Card.Text>
-									<BiPlus className="plus" />
-								</Card.Body>
-							</Card>
-						</Col>
-						<Col md={6}>
-							<Card
-								className="card"
-								style={{
-									backgroundImage: "url('/images/Desktop/trekker-cover.png')",
-								}}
-							>
-								<Card.Body>
-									<Card.Title className="card-title">Trekker</Card.Title>
-									<Card.Text className="card-text">UI/UX Design</Card.Text>
-									<BiPlus className="plus" />
-								</Card.Body>
-							</Card>
-						</Col>
+						{cardsData.slice(0, 2).map((card) => (
+							<Col md={6} key={card.id}>
+								<Card
+									className="card"
+									style={{
+										backgroundImage: `url(${card.image})`,
+									}}
+								>
+									<Card.Body>
+										<Card.Title className="card-title">{card.title}</Card.Title>
+										<Card.Text className="card-text">
+											{card.description}
+										</Card.Text>
+										<BiPlus
+											className="work-plus"
+											onClick={() => handleOpenModal(card.modalContent)}
+										/>
+									</Card.Body>
+								</Card>
+							</Col>
+						))}
 					</Row>
 					<Row style={{ height: "375px" }}>
-						<Col md={6}>
-							<Card
-								className="card"
-								style={{
-									backgroundImage: "url('/images/Desktop/ecoenergy-cover.png')",
-								}}
-							>
-								<Card.Body>
-									<Card.Title className="card-title">Ecco Energy</Card.Title>
-									<Card.Text className="card-text">Logo Design</Card.Text>
-									<BiPlus className="plus" />
-								</Card.Body>
-							</Card>
-						</Col>
-						<Col md={6}>
-							<Card
-								className="card"
-								style={{
-									backgroundImage:
-										"url('/images/Desktop/mindfulsouls-cover.png')",
-								}}
-							>
-								<Card.Body className="card-body">
-									<Card.Title className="card-title">Mindful Sould</Card.Title>
-									<Card.Text className="card-text">Website redesign</Card.Text>
-									<BiPlus className="plus" />
-								</Card.Body>
-							</Card>
-						</Col>
+						{cardsData.slice(2).map((card) => (
+							<Col md={6} key={card.id}>
+								<Card
+									className="card"
+									style={{
+										backgroundImage: `url(${card.image})`,
+									}}
+								>
+									<Card.Body>
+										<Card.Title className="card-title">{card.title}</Card.Title>
+										<Card.Text className="card-text">
+											{card.description}
+										</Card.Text>
+										<BiPlus
+											className="work-plus"
+											onClick={() => handleOpenModal(card.modalContent)}
+										/>
+									</Card.Body>
+								</Card>
+							</Col>
+						))}
 					</Row>
+					<ModalOverlay
+						showModal={showModal}
+						handleCloseModal={handleCloseModal}
+						content={modalContent}
+					/>
 				</Container>
 			) : (
 				<Container>
