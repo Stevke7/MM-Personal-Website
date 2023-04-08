98948ba55c478e7970f42d4270cadac70f781526
Stevanovic <stevanovicljubisa07@gmail.com>
2023-04-08 23:23:45 +0200

    styling and adding content to modal overlay

--- src/pages/home/components/modal-overlay/modal-overlay.js
+++ src/pages/home/components/modal-overlay/modal-overlay.js
@@ -20,15 +20,10 @@ function ModalOverlay({ showModal, handleCloseModal, title, content }) {
 				onHide={handleCloseModal}
 				onScroll={handleScroll}
 			>
-				{/* <Modal.Header className="header" closeButton></Modal.Header> */}
-
-				<Modal.Body className="body">
+				<Modal.Body style={{ padding: 0 }}>
 					{content === "Content for Cheetah Atletic modal" && (
-						<div
-							className="main-div"
-							style={{ display: "flex", flexDirection: "row" }}
-						>
-							<div className="cheetah">
+						<div className="main-div">
+							<div className="cover">
 								<div className="helpers">
 									<ProgressBar
 										now={scrollPercentage}
@@ -38,16 +33,40 @@ function ModalOverlay({ showModal, handleCloseModal, title, content }) {
 									<BiPlus onClick={handleCloseModal} className="plusic" />
 								</div>
 							</div>
-							<div>
-								<div className="contents">
-									<h1>Unleashe Your inner Athlete</h1>
-									<h6>
-										Cheetah Athletics is a sports and apparel club that provides
-										everything you need to achieve your sports & fitness goals.
-										It pushes people to live an active and healthy lifestyle by
-										training, eating and wearing better.
-									</h6>
+
+							<div className="contents">
+								<div className="screen-1">
+									<div className="frame-1">
+										<h1>Unleash Your inner Athlete</h1>
+										<p>
+											Cheetah Athletics is a sports and apparel club that
+											provides everything you need to achieve your sports &
+											fitness goals. It pushes people to live an active and
+											healthy lifestyle by training, eating and wearing better.
+										</p>
+									</div>
+									<div className="frame-2">
+										<p>
+											Cheetahs are the world’s fastest land animal, capable of
+											reaching speeds of up to 70mph. They are built for speed,
+											grace, and hunting. Cheetah Athletics encompass the spirit
+											of cheetah and live it’s moto - Born to run!
+										</p>
+										<div className="image"></div>
+									</div>
 								</div>
+								{/* <div className="screen-2">
+									<div className="images-row">
+										<div className="image1"></div>
+										<div className="image2"></div>
+										<div className="image3"></div>
+									</div>
+									<p>
+										The logo is created by combining a cheetah silhouette, with
+										the lettermarks enclosed in a circular shape. The circle
+										represents stability, continuity and perseverance.
+									</p>
+								</div> */}
 							</div>
 						</div>
 					)}
