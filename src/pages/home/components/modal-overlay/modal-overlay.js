dc5d50c6e036f0c5f00c3c9cbda4ba9db083f463
Stevanovic <stevanovicljubisa07@gmail.com>
2023-05-16 22:49:32 +0200

    Cheetah project modal overlay

--- src/pages/home/components/modal-overlay/modal-overlay.js
+++ src/pages/home/components/modal-overlay/modal-overlay.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Modal, ProgressBar } from "react-bootstrap";
+import { TbX } from "react-icons/tb";
 import { BiPlus } from "react-icons/bi";
 
 function ModalOverlay({ showModal, handleCloseModal, title, content }) {
@@ -30,21 +31,23 @@ function ModalOverlay({ showModal, handleCloseModal, title, content }) {
 										label={`${Math.round(scrollPercentage)}%`}
 										className="progress-bar"
 									/>
-									<BiPlus onClick={handleCloseModal} className="plusic" />
+									<div className="close-btn">
+										<TbX onClick={handleCloseModal} className="plusic" />
+									</div>
 								</div>
 							</div>
 
 							<div className="contents">
-								<div className="screen-1">
-									<div className="frame-1">
-										<h1>Unleash Your inner Athlete</h1>
-										<p>
-											Cheetah Athletics is a sports and apparel club that
-											provides everything you need to achieve your sports &
-											fitness goals. It pushes people to live an active and
-											healthy lifestyle by training, eating and wearing better.
-										</p>
-									</div>
+								<div className="section-1">
+									<h1>Unleash Your inner Athlete</h1>
+									<p>
+										Cheetah Athletics is a sports and apparel club that provides
+										everything you need to achieve your sports & fitness goals.
+										It pushes people to live an active and healthy lifestyle by
+										training, eating and wearing better.
+									</p>
+								</div>
+								<div className="section-2">
 									<div className="frame-2">
 										<p>
 											Cheetahs are the world’s fastest land animal, capable of
@@ -55,18 +58,60 @@ function ModalOverlay({ showModal, handleCloseModal, title, content }) {
 										<div className="image"></div>
 									</div>
 								</div>
-								{/* <div className="screen-2">
+								<div className="section-3">
 									<div className="images-row">
 										<div className="image1"></div>
 										<div className="image2"></div>
 										<div className="image3"></div>
 									</div>
-									<p>
+									<p className="section-3-paragraph">
 										The logo is created by combining a cheetah silhouette, with
 										the lettermarks enclosed in a circular shape. The circle
 										represents stability, continuity and perseverance.
 									</p>
-								</div> */}
+								</div>
+								<div className="section-4">
+									<div className="section-4-text">
+										<h2>Train good.</h2>
+										<h2>Eat good.</h2>
+										<h2>Look good.</h2>
+									</div>
+
+									<div className="section-4-images-row">
+										<div className="section-4-image-1"></div>
+										<div className="section-4-image-2"></div>
+										<div className="section-4-image-3"></div>
+									</div>
+								</div>
+								<div className="section-5">
+									<div className="section-5-images-row">
+										<div className="image-top"></div>
+										<div className="image-bottom"></div>
+									</div>
+									<p>
+										Cheetah Athletics is a sports and apparel club that provides
+										everything you need to achieve your sports & fitness goals.
+									</p>
+								</div>
+								<div className="section-6">
+									<div className="section-6-top-row">
+										<h2>
+											<span>Perfect fit</span> and harmony between sports and
+											technology
+										</h2>
+										<p>Cheetah Athletics is a sports and apparel club.</p>
+									</div>
+									<div className="section-6-image-row"></div>
+								</div>
+								<div className="section-7">
+									<div className="section-7-top-row">
+										<div className="top-image-7"></div>
+									</div>
+									<div className="section-7-bottom-row">
+										<div className="image-left"></div>
+										<div className="image-right"></div>
+									</div>
+								</div>
 							</div>
 						</div>
 					)}
