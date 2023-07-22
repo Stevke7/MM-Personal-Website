1e5bdd2e9675e6715615ee752168a318789e1e62
Nikola <nikolajamina33@gmail.com>
2023-07-22 13:25:33 +0200

    Mobile optimization

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,12 +1,16 @@
-import {Route, Routes} from "react-router-dom";
-import React from "react";
+import { Route, Routes } from "react-router-dom";
+import React, { useState } from "react";
 import Home from "../pages/home/Home";
 
+
 function SiteRoutes(props) {
-    let {id, scrollToSection,contactRef, workRef, isMobile} = props
+    let { id, scrollToSection, contactRef, workRef, isMobile } = props
+
     return (
         <Routes>
-            <Route path="/" element={<Home isMobile={isMobile} contactRef={contactRef} workRef={workRef} scrollToSection={scrollToSection} id={id}/>}/>
+            <Route path="/" element={<Home isMobile={isMobile} contactRef={contactRef} workRef={workRef} scrollToSection={scrollToSection} id={id} />} />
+
+
         </Routes>
     )
 }
